9471adb19969a53369d262b35eab9c24d2d9d24e
mondalwahid <mondalwahid@gmail.com>
2023-12-30 12:56:17 +0530

    removed unused code blocks

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -17,38 +17,6 @@ import CategoriesIcon from "react-native-vector-icons/MaterialCommunityIcons";
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-const CustomTabBarButton = ({
-  accessibilityLabel,
-  onPress,
-  onLongPress,
-  icon,
-  isFocused,
-}) => (
-  <TouchableOpacity
-    style={{
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-      elevation: isFocused ? 5 : 0, // Apply elevation only when the tab is focused
-    }}
-    onPress={onPress}
-    onLongPress={onLongPress}
-  >
-    <View
-      style={{
-        width: 30,
-        height: 30,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "white",
-        borderRadius: 15,
-      }}
-    >
-      {icon}
-    </View>
-  </TouchableOpacity>
-);
-
 const HomeStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
@@ -64,6 +32,7 @@ const TabNavigation = () => {
     <Tab.Navigator
       initialRouteName="Home"
       screenOptions={({ route }) => ({
+        tabBarVisible: route.name !== "ProductDetails",
         headerShown: false,
         tabBarShowLabel: false,
         tabBarStyle: {
